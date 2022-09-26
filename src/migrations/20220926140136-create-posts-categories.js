b5e570d18a0b883a08c8420f1047e5e286424d8e
Mateus <mateusfpleite@gmail.com>
2022-09-26 11:27:36 -0300

    cria migration de post_categories

---
+++ src/migrations/20220926140136-create-posts-categories.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', 
+    { 
+      post_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        }
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'categories',
+          key: 'id',
+        }
+      },
+     });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
