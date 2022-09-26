eb4dbc5f91def1507a4948a6b887a1736470a5f9
Mateus <mateusfpleite@gmail.com>
2022-09-26 11:26:06 -0300

    cria model de User

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,18 @@
 const UserModel = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-      fullName: DataTypes.STRING,
+      id: DataTypes.INTEGER,
+      display_name: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       image: DataTypes.STRING,
-    });
+    },
+    {timestamps: false,
+    tableName: 'users'});
+    
+    User.associate = (models) => {
+      User.hasMany(models.blogPosts,
+        { foreignKey: 'user_id', as: 'blog_posts' });
+    };
   
     return User;
   };
