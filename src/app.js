2b9a35f3b47f978d2199bad83d68c477f66050da
Mateus <mateusfpleite@gmail.com>
2022-09-26 16:42:05 -0300

    adiciona controller em app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const User = require('./controller/User');
+const validateLogin = require('./middlewares/loginValidation');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', validateLogin, User.getUser);
 
 // ...
 
