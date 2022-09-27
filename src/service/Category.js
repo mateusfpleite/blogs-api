fdbf121960c8a9ec2b4a2e8e9182dcf989178175
Mateus <mateusfpleite@gmail.com>
2022-09-27 19:12:39 -0300

    cria service de Category e a função
    createCategory

---
+++ src/service/Category.js
@@ -0,0 +1,8 @@
+const { Category } = require('../models');
+
+const createCategory = async (name) => {
+const result = await Category.create({ name });
+return result;
+};
+
+module.exports = { createCategory };
