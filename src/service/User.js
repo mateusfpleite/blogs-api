70d8d9a263ec01dbb734d701432112786897971e
Mateus <mateusfpleite@gmail.com>
2022-09-27 19:12:10 -0300

    remove console.log na linha 35

--- src/service/User.js
+++ src/service/User.js
@@ -32,7 +32,6 @@ const getAllUsers = async () => {
 
 const getUserById = async (id) => {
     const result = await User.findByPk(id, { attributes: { exclude: ['password'] } });
-    console.log(result);
     if (!result) { return { type: 'NOT_FOUND', message: 'User does not exist' }; }
     return { type: null, message: result };
 };
