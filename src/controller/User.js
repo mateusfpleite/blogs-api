a6a3b212e59861bb76478b18ccaf9d51ae47158f
Mateus <mateusfpleite@gmail.com>
2022-09-27 17:47:27 -0300

    corrige bug adicionando return na linha 6

--- src/controller/User.js
+++ src/controller/User.js
@@ -3,7 +3,7 @@ const UserService = require('../service/User');
 const getUser = async (req, res) => {
     const { email, password } = req.body;
     const result = await UserService.getUser(email, password);
-    if (result.type) { res.status(400).json({ message: result.message }); }
+    if (result.type) { return res.status(400).json({ message: result.message }); }
     return res.status(200).json({ token: result.message });
 };
 
