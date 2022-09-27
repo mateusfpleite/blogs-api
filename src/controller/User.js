2ecda7af302796dfa3dbae16303726ad06ffd1f6
Mateus <mateusfpleite@gmail.com>
2022-09-27 10:43:25 -0300

    cria a função createUser na controller User.js

--- src/controller/User.js
+++ src/controller/User.js
@@ -1,11 +1,17 @@
-const UserService = require('../service/User'); 
+const UserService = require('../service/User');
 
 const getUser = async (req, res) => {
-const { email, password } = req.body;
-const result = await UserService.getUser(email, password);
-console.log(result);
-if (result.type) { res.status(400).json({ message: result.message }); }
-return res.status(200).json({ token: result.message });
+    const { email, password } = req.body;
+    const result = await UserService.getUser(email, password);
+    if (result.type) { res.status(400).json({ message: result.message }); }
+    return res.status(200).json({ token: result.message });
 };
 
-module.exports = { getUser };
+const createUser = async (req, res) => {
+    const { displayName, email, password, image } = req.body;
+    const result = await UserService.createUser(displayName, email, password, image);
+    if (result.type) return res.status(409).json({ message: result.message });
+    return res.status(201).json({ token: result.message });
+};
+
+module.exports = { getUser, createUser };
